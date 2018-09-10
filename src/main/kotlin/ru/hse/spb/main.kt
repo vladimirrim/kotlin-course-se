3c9a35c0649ea1b09d97a951d7ddbe654e92649f
vladimirrim <vladimirrim98@gmail.com>
2018-09-10 23:04:38 +0300

    version 1.0

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,60 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+fun getSuffix(str: String): String {
+    return when {
+        str.endsWith("etr") -> "etr"
+        str.endsWith("etra") -> "etra"
+        str.endsWith("initis") -> "initis"
+        str.endsWith("inites") -> "inites"
+        str.endsWith("lios") -> "lios"
+        str.endsWith("liala") -> "liala"
+        else -> ""
+    }
+}
 
-    return words.joinToString(separator = " ")
+fun checkSameGender(prev: String, cur: String): Boolean {
+    val woman = arrayOf("etra", "inites", "liala")
+    val man = arrayOf("etr", "initis", "lios")
+
+    return (prev in man && cur in man) || (prev in woman && cur in woman)
+}
+
+
+fun checkOrder(prevString: String, curString: String): Boolean {
+    val nouns = arrayOf("etr", "etra")
+    val verbs = arrayOf("initis", "inites")
+    val adjs = arrayOf("lios", "liala")
+
+    return when (prevString) {
+        "" -> true
+
+        "etr", "etra", "initis", "inites" -> curString in verbs && checkSameGender(prevString, curString)
+
+        "lios", "liala" -> (curString in nouns || curString in adjs) && checkSameGender(prevString, curString)
+
+        else -> false
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    var prevString = ""
+    var ans = true
+    var wordCount = 0
+    var hasNoun = false
+    val nouns = arrayOf("etr", "etra")
+
+    readLine()!!.split(' ').forEach { curString ->
+        val suf = getSuffix(curString)
+        if (suf == "" || !checkOrder(prevString, suf)) {
+            ans = false
+        }
+        wordCount++
+        if (suf in nouns)
+            hasNoun = true
+        prevString = suf
+    }
+    if (ans && (wordCount == 1 || hasNoun))
+        print("YES")
+    else
+        print("NO")
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,56 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.io.PrintStream
+import java.io.ByteArrayOutputStream
+
+
 
 class TestSource {
-    @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+
+    private val outContent = ByteArrayOutputStream()
+    private val errContent = ByteArrayOutputStream()
+    private val originalOut = System.out
+    private val originalErr = System.err
+
+    @BeforeEach
+    fun setUpStreams() {
+        System.setOut(PrintStream(outContent))
+        System.setErr(PrintStream(errContent))
     }
+
+    @AfterEach
+    fun restoreStreams() {
+        System.setOut(originalOut)
+        System.setErr(originalErr)
+    }
+
+    @Test
+    fun mainTestOneWord() {
+        System.setIn(ByteArrayInputStream("petr".toByteArray()))
+        main(arrayOf())
+        assertEquals("YES", outContent.toString())
+    }
+
+    @Test
+    fun mainTestCorrectSentence() {
+        System.setIn(ByteArrayInputStream("nataliala kataliala vetra feinites".toByteArray()))
+        main(arrayOf())
+        assertEquals("YES", outContent.toString())
+    }
+
+    @Test
+    fun mainTestIncorrectSentence() {
+        System.setIn(ByteArrayInputStream("etis atis animatis etis atis amatis".toByteArray()))
+        main(arrayOf())
+        assertEquals("NO", outContent.toString())
+    }
+
+
+
+
 }
